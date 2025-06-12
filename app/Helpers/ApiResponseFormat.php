4cbe4f54d53850ab3b42fd1bc50ace492425814a
Omar-Ayman-Gaber <omarfcinews@gmail.com>
2025-06-12 18:13:16 +0300

    chore: Install Microsoft auth account provider

---
+++ app/Helpers/ApiResponseFormat.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Helpers;
+
+class ApiResponseFormat
+{
+    public static function successResponse($responseCode, $responseMessage, $responseData)
+    {
+        $response = [
+            'statusCode' => $responseCode,
+            'message' => $responseMessage,
+            'clientData' => $responseData,
+        ];
+        return response()->json($response, $responseCode);
+    }
+    public static function failedResponse($responseCode, $responseMessage, $responseErrors)
+    {
+        $response = [
+            'statusCode' => $responseCode,
+            'message' => $responseMessage,
+            'errors' => $responseErrors,
+        ];
+        return response()->json($response, $responseCode);
+    }
+}
