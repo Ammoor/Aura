674db4669e120b4d9ca269b23468033c8ea016a3
Omar-Ayman-Gaber <omarfcinews@gmail.com>
2025-07-22 19:16:52 +0300

    fix: Resolve bug when retrieving document and user data from the bucket

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -8,12 +8,6 @@ use Illuminate\Support\Facades\Storage;
 
 class UserResource extends JsonResource
 {
-    private string $appName;
-    public function __construct($resource)
-    {
-        parent::__construct($resource);
-        $this->appName = config('app.name');
-    }
     /**
      * Transform the resource into an array.
      *
@@ -22,7 +16,7 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'userProfileImage' => Storage::disk('s3')->url($this->appName . '/' . $this->profile_image_path),
+            'userProfileImage' => Storage::disk('s3')->url($this->profile_image_path),
             'userFirstName' => $this->first_name,
             'userLastName' => $this->last_name,
             'userEmail' => $this->email,
