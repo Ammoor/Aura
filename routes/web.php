7b424e1d9e8976531ce5f35d1c25fae174161984
Omar-Ayman-Gaber <omarfcinews@gmail.com>
2025-08-02 18:22:07 +0300

    revert: Remove the Api Key middleware from the web routes file

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthAccountController;
-use App\Http\Middleware\ApiKeyMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(ApiKeyMiddleware::class)->group(function () {
+Route::controller(AuthAccountController::class)->group(function () {
 
-    Route::controller(AuthAccountController::class)->group(function () {
+    Route::get('{provider_name}/redirect', 'redirect');
 
-        Route::get('{provider_name}/redirect', 'redirect');
-
-        Route::get('{provider_name}/callback', 'callback');
-    });
+    Route::get('{provider_name}/callback', 'callback');
 });
